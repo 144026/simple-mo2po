0a3a684d9c207f651af91389efc973a5612f03d6
144026 <you@example.com>
2021-02-03 03:03:02 +0800

    A: mo_header_t, GetMoHeader()

--- src/ExtractPO.c
+++ src/ExtractPO.c
@@ -3,25 +3,37 @@
 #include<stdlib.h>
 #include<string.h>
 
-int ShowMoHeader(FILE * fpMo){
-    unsigned int magic, str_num;
+typedef struct _mo_header
+{
+    unsigned int magic;
+    unsigned int str_num;
     unsigned int OffsetTableOfOrigin;
     unsigned int OffsetTableOfTrans;
-    unsigned int SizeOfHash, OffsetOfHash;
+    unsigned int SizeOfHash;
+    unsigned int OffsetOfHash;
+} mo_header_t, *p_mo_header_t;
 
+int GetMoHeader(FILE * fpMo, p_mo_header_t pMoHeader){
     fseek(fpMo,0,SEEK_SET);
-    fread(&magic,sizeof(char),4,fpMo);
+    fread(&pMoHeader->magic,sizeof(char),4,fpMo);
 
     fseek(fpMo,8,SEEK_SET);
-    fread(&str_num,sizeof(char),4,fpMo);
+    fread(&pMoHeader->str_num,sizeof(char),4,fpMo);
 
-    fread(&OffsetTableOfOrigin,sizeof(char),4,fpMo);
-    fread(&OffsetTableOfTrans,sizeof(char),4,fpMo);
+    fread(&pMoHeader->OffsetTableOfOrigin,sizeof(char),4,fpMo);
+    fread(&pMoHeader->OffsetTableOfTrans,sizeof(char),4,fpMo);
 
-    fread(&SizeOfHash,sizeof(char),4,fpMo);
-    fread(&OffsetOfHash,sizeof(char),4,fpMo);
+    fread(&pMoHeader->SizeOfHash,sizeof(char),4,fpMo);
+    fread(&pMoHeader->OffsetOfHash,sizeof(char),4,fpMo);
+}
 
-    printf("magic: 0x%x \tstr_num: %u\n",magic,str_num);
-    printf("off_tab_origin: 0x%x \toff_tab_trans: 0x%x\n",OffsetTableOfOrigin,OffsetTableOfTrans);
-    printf("size_of_hash: 0x%x \toff_of_hash: 0x%x\n",SizeOfHash,OffsetOfHash);
+int ShowMoHeader(FILE * fpMo){
+    mo_header_t MoHeader;
+    GetMoHeader(fpMo, &MoHeader);
+
+    printf("magic: 0x%x \tstr_num: %u\n",MoHeader.magic,MoHeader.str_num);
+    printf("off_tab_origin: 0x%x \toff_tab_trans: 0x%x\n",
+        MoHeader.OffsetTableOfOrigin,MoHeader.OffsetTableOfTrans);
+    printf("size_of_hash: 0x%x \toff_of_hash: 0x%x\n",
+        MoHeader.SizeOfHash,MoHeader.OffsetOfHash);
 }
